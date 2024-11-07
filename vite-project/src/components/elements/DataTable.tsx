1b5dd6cbd3ee45730a6ff9821cd1d325d4bdfc4f
mitej23 <mitejmadan@gmail.com>
2024-11-07 17:00:16 +0530

    fix: table row height

--- vite-project/src/components/elements/DataTable.tsx
+++ vite-project/src/components/elements/DataTable.tsx
@@ -93,7 +93,7 @@ export function DataTable<TData, TValue>({
                   <TableRow key={headerGroup.id}>
                     {headerGroup.headers.map((header) => {
                       return (
-                        <TableHead key={header.id}>
+                        <TableHead key={header.id} className="h-11">
                           {header.isPlaceholder
                             ? null
                             : flexRender(
@@ -113,7 +113,7 @@ export function DataTable<TData, TValue>({
                       key={row.id}
                       data-state={row.getIsSelected() && "selected"}>
                       {row.getVisibleCells().map((cell) => (
-                        <TableCell key={cell.id}>
+                        <TableCell key={cell.id} className="py-2.5">
                           {flexRender(
                             cell.column.columnDef.cell,
                             cell.getContext()
